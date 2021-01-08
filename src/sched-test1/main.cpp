7c9d37bd296fe3601d5e9c2cb10293be4b62eb59
Tom Spink <tspink@gmail.com>
2021-01-08 15:28:49 +0000

    Add in an extra debugging line for the scheduler test

--- src/sched-test1/main.cpp
+++ src/sched-test1/main.cpp
@@ -5,21 +5,22 @@
 static void thread_proc(void *arg)
 {
 	unsigned int thread_num = (unsigned int)(unsigned long)arg;
-	
+
 	printf("Thread %d Running!\n", thread_num);
-	
+
 	usleep(1500000 + (thread_num * 500000));
-	
+
 	printf("Thread %d Stopping!\n", thread_num);
-	
+
 	stop_thread(HTHREAD_SELF);
 }
 
 int main(const char *cmdline)
 {
 	printf("Running Scheduler Test!\n");
-	
+
 	HTHREAD threads[10];
+	printf("Using %d threads...\n", ARRAY_SIZE(threads));
 	for (unsigned int i = 0; i < ARRAY_SIZE(threads); i++) {
 		threads[i] = create_thread(thread_proc, (void *)(unsigned long)i);
 	}
@@ -28,8 +29,8 @@ int main(const char *cmdline)
 		printf("Waiting for thread %d...\n", i);
 		join_thread(threads[i]);
 	}
-	
+
 	printf("Scheduler Test Complete\n");
-	
+
 	return 0;
 }
