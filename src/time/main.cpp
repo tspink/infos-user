e800b8a4091b37dedf5a2967f875c8b43cfc9ee8
Tom Spink <tspink@gmail.com>
2021-02-15 09:59:25 +0000

    Make time work with command-line arguments

--- src/time/main.cpp
+++ src/time/main.cpp
@@ -8,17 +8,27 @@ int main(const char *cmdline)
 		return 1;
 	}
 
+	const char *cmd = cmdline;
+	char prog[64];
+	int n = 0;
+	while (*cmd && *cmd != ' ' && n < 63) {
+		prog[n++] = *cmd++;
+	}
+	prog[n] = 0;
+
+	if (*cmd) cmd++;
+
 	uint64_t start = get_ticks();
 
-	HPROC pcmd = exec(cmdline, "");
+	HPROC pcmd = exec(prog, cmd);
 	if (is_error(pcmd)) {
-		printf("error: unable to run command '%s'\n", cmdline);
+		printf("error: unable to run command '%s' %s\n", cmdline, cmd);
 	} else {
 		wait_proc(pcmd);
 
 		uint64_t end = get_ticks();
 		uint64_t delta = end - start;
-		printf("real time: %lu ns\n", delta);
+		printf("real time: %lu ms\n", delta/1000);
 	}
 
 	return 0;
