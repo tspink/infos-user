c8f7308b195f86c2f2d52a118e64dd587b8b8f66
Kim Stonehouse <kimbethstonehouse@gmail.com>
2022-01-09 18:48:03 +0000

    Add priorities to sched-test1, not working yet

--- src/sched-test1/main.cpp
+++ src/sched-test1/main.cpp
@@ -2,6 +2,8 @@
 
 #include <infos.h>
 
+static volatile bool terminate;
+
 static void thread_proc(void *arg)
 {
 	unsigned int thread_num = (unsigned int)(unsigned long)arg;
@@ -19,15 +21,67 @@ static void thread_proc(void *arg)
 	stop_thread(HTHREAD_SELF);
 }
 
+static void ticker1_thread_proc(void *arg)
+{
+    printf("Thread 1 ticking every 1.5s\n");
+    while (!terminate) {
+        printf("TICK 1!\n");
+        usleep(1500000);
+    }
+
+    stop_thread(HTHREAD_SELF);
+}
+
+static void ticker2_thread_proc(void *arg)
+{
+    printf("Thread 2 ticking every 1s\n");
+    while (!terminate) {
+        printf("TICK 2!\n");
+        usleep(1000000);
+    }
+
+    stop_thread(HTHREAD_SELF);
+}
+
+static void terminate_ticker_thread_proc(void *arg)
+{
+    printf("Press <ENTER> to stop\n");
+    while (getch() != '\n');
+    printf("Stopping threads...\n");
+    terminate = true;
+
+    stop_thread(HTHREAD_SELF);
+}
+
+static void fibonacci_thread_proc(void *arg) {
+    int f1 = 0;
+    int f2 = 1;
+    int fi;
+    for (int i = 0; i < 500; i++) {
+        fi = f1 + f2;
+        f1 = f2;
+        f2 = fi;
+        printf("F%u: %u\n", i, fi);
+    }
+}
+
 int main(const char *cmdline)
 {
 	printf("Running Scheduler Test!\n");
 
-	HTHREAD threads[10];
+    terminate = false;
+
+    HTHREAD threads[14];
 	printf("Using %d threads...\n", ARRAY_SIZE(threads));
-	for (unsigned int i = 0; i < ARRAY_SIZE(threads); i++) {
-		threads[i] = create_thread(thread_proc, (void *)(unsigned long)i);
+	for (unsigned int i = 0; i < 10; i++) {
+		threads[i] = create_thread(thread_proc, (void *)(unsigned long)i, SchedulingEntityPriority::REALTIME);
 	}
+
+    threads[10] = create_thread(terminate_ticker_thread_proc, nullptr, SchedulingEntityPriority::INTERACTIVE);
+    threads[11] = create_thread(ticker1_thread_proc, nullptr, SchedulingEntityPriority::INTERACTIVE);
+    threads[12] = create_thread(ticker2_thread_proc, nullptr, SchedulingEntityPriority::INTERACTIVE);
+
+    threads[13] = create_thread(fibonacci_thread_proc, nullptr, SchedulingEntityPriority::DAEMON);
 
 	for (unsigned int i = 0; i < ARRAY_SIZE(threads); i++) {
 		printf("Waiting for thread %d...\n", i);
